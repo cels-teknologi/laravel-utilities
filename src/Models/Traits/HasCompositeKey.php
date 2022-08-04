8bbedc38f3680175d3980977153da673913b173e
Stephen Dewanto <stephen.dewanto@gmail.com>
2022-08-04 15:50:25 +0700

    Initial commit

---
+++ src/Models/Traits/HasCompositeKey.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Cels\Utilities\Models\Traits;
+
+use InvalidArgumentException;
+
+trait HasCompositeKey
+{
+    /**
+     * Get the composite primary keys for the model,
+     * or "force" a composite key from primary key.
+     *
+     * @return array
+     */
+    public function getKeyNames()
+    {
+        if (!is_array($this->primaryKey)) {
+            return [$this->primaryKey, ];
+        }
+
+        return $this->primaryKey;
+    }
+
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function setKeysForSaveQuery($query)
+    {
+        if (!is_array($this->primaryKey)) {
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        $keys = $this->getKeyNames();
+        foreach ($keys as $keyName) {
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the composite key values for a save query.
+     *
+     * @param ?string $keyName
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if (is_null($keyName)) {
+            throw new InvalidArgumentException('\'keyName\' argument cannot be null for models with composite key.');
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
+}
---
+++ src/Models/Traits/HasUuidPrimary.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Cels\Utilities\Models\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasUuidPrimary
+{
+    /**
+     * Generates UUID for a given column name / key name.
+     *
+     * @param  string  $keyName 
+     * @return string
+     */
+    public function generateUuid($keyName = null)
+    {
+        return Str::orderedUuid();
+    }
+
+    /**
+     * Boot UUID primary trait.
+     *
+     * @return void
+     */
+    protected static function bootHasUuidPrimary()
+    {
+        // parent::boot();
+
+        static::creating(function ($model) {
+            if ($model instanceof HasCompositeKey) {
+                $keys = $model->getKeyNames();
+                foreach ($keys as $keyName) {
+                    if (!empty($model->{$keyName})) {
+                        continue;
+                    }
+
+                    $model->{$keyName} = (string) $model->generateUuid($keyName);
+                }
+            }
+            else {
+                $key = $model->getKeyName();
+                $model->{$key} = (string) $model->generateUuid($key);
+            }
+        });
+    }
+}
